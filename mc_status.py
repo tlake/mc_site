4e69c8b7a4e158c1354d09a99d89628b4ad767be
tlake <tanner.lake@gmail.com>
2016-02-03 18:28:18 -0800

    View finished!
    - View now queries db to retrieve servers
    - Template updates to display information from view
    - CSS tweaks
      - Removal of unused CSS files generated by Compass
      - Renamed screen to default for .sass (and thus .css) file(s)

--- mc_status.py
+++ mc_status.py
@@ -6,9 +6,13 @@ information.
 """
 
 from flask import Flask, render_template
+from flask.ext.misaka import Misaka
 from flask.ext.sqlalchemy import SQLAlchemy
+
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+
+from mcstatus import MinecraftServer
 
 
 ################################
@@ -16,6 +20,7 @@ from flask_admin.contrib.sqla import ModelView
 ################################
 
 app = Flask(__name__)
+Misaka(app)
 app.config.update(
     DEBUG=True,
     SECRET_KEY="man, who knows?",
@@ -34,8 +39,9 @@ class Server(db.Model):
     """Server model."""
 
     id = db.Column(db.Integer, primary_key=True)
-    server_address = db.Column(db.String(16))
-    server_port = db.Column(db.String(8))
+    address = db.Column(db.String(16))
+    port = db.Column(db.Integer)
+    modpack_version = db.Column(db.String(64))
     client_config = db.Column(db.Text)
     created = db.Column(
         db.DateTime,
@@ -61,8 +67,51 @@ admin.add_view(ModelView(Server, db.session))
 @app.route("/")
 def index():
     """Index (and only) page of site."""
-    servers = {"testkey": "testvalue"}
-    return render_template("index.html", servers=servers)
+    server_objects = Server.query.all()
+    render_obj = []
+
+    for server in server_objects:
+        address = server.address
+        port = server.port
+        conn = MinecraftServer(address, port)
+
+        try:
+            conn.ping()
+            add_to_render = {}
+            q = conn.query()
+
+            # import pdb; pdb.set_trace()
+
+            motd = q.motd
+            players = {
+                "max": q.players.max,
+                "online": q.players.online,
+                "names": q.players.names,
+            }
+            jar_version = q.software.version
+
+            add_to_render.update({
+                "address": address,
+                "port": port,
+                "modpack_version": server.modpack_version,
+                "client_config": server.client_config,
+                "status": "Online",
+                "motd": motd,
+                "players": players,
+                "jar_version": jar_version,
+            })
+
+            render_obj.append(add_to_render)
+        except ConnectionRefusedError:
+            render_obj.append({
+                "address": address,
+                "port": port,
+                "modpack_version": server.modpack_version,
+                "client_config": server.client_config,
+                "status": "Offline",
+            })
+
+    return render_template("index.html", servers=render_obj)
 
 
 if __name__ == "__main__":
